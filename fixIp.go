2afcd5eef19c2a52930e6f87174d928d4559055a
Bhuridech Sudsee <dekcomgang@hotmail.com>
2015-10-23 13:23:36 +0700

    feature request and unmarshall

--- fixIp.go
+++ fixIp.go
@@ -6,12 +6,19 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+//	"strings"
 //	"net/http/httptest"
 //	"net/url"
 //	"github.com/parnurzeal/gorequest"
 )
 
+type ResponseAuth struct {
+		Redirect	string `json:"redirect"`
+		Error		string `json:"error"`
+	}
+
 func main() {
+	var err error
 	credential := map[string]interface{}{
 		"username": "admin",
 		"password": "",
@@ -20,7 +27,7 @@ func main() {
 	credentialJson, _ := json.Marshal(credential)
 	contentReader := bytes.NewReader(credentialJson)
 	req, _ := http.NewRequest("POST", "http://192.168.1.1/htdocs/login/login.lua", contentReader)
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Notes","Get MAC address form switch port")
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -30,7 +37,13 @@ func main() {
 	defer resp.Body.Close()
 	body,err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error ReadAll:", err)
 	}
-	fmt.Println(string(body))
+	var responseAuth ResponseAuth
+	err = json.Unmarshal(body, &responseAuth)
+	if err != nil {
+		fmt.Println("error Unmarshal:", err)
+	}
+	fmt.Printf("Redirect: %v\n", responseAuth.Redirect)
+	fmt.Printf("Error: %v\n", responseAuth.Error)
 }
